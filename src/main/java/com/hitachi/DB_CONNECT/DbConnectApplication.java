af73f28a7f8e54f738f7ac8041d2422c0c8d95cd
Mohamed Karam <mkaram@TradeNET.COM>
2024-10-16 15:53:27 +0300

    Initial commit

---
+++ src/main/java/com/hitachi/DB_CONNECT/DbConnectApplication.java
@@ -0,0 +1,13 @@
+package com.hitachi.DB_CONNECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitachi/DB_CONNECT/DbConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitachi.DB_CONNECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
