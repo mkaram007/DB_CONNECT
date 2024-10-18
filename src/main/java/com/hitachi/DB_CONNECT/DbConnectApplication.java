16128108953d350d61f0219b18f2c8fb2637cb0a
Mohamed Karam <mkaram@TradeNET.COM>
2024-10-18 07:03:04 +0300

    Setting db connection, defining the working method

--- src/main/java/com/hitachi/DB_CONNECT/DbConnectApplication.java
+++ src/main/java/com/hitachi/DB_CONNECT/DbConnectApplication.java
@@ -2,12 +2,17 @@ package com.hitachi.DB_CONNECT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) 
+// @SpringBootApplication
+// "@GetMapping"
 public class DbConnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbConnectApplication.class, args);
 	}
-
+	// "@GetMapping"
+	public String working() {
+		return "It's working";
+	}
 }
