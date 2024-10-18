abaeb7a7cff11f24e8a938320cb991595b4e21b9
Mohamed Karam <mkaram@TradeNET.COM>
2024-10-18 09:07:40 +0300

    First working code

--- src/main/java/com/hitachi/DB_CONNECT/DbConnectApplication.java
+++ src/main/java/com/hitachi/DB_CONNECT/DbConnectApplication.java
@@ -3,15 +3,14 @@ package com.hitachi.DB_CONNECT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) 
-// @SpringBootApplication
-// "@GetMapping"
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
 public class DbConnectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DbConnectApplication.class, args);
 	}
-	// "@GetMapping"
+	@GetMapping
 	public String working() {
 		return "It's working";
 	}
